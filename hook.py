9b921a5f710665f37a02e878d53dd8d926b0a511
Adokr <aj.okrasinsk@student.uw.edu.pl>
2025-05-14 17:58:59 +0200

    qq

--- hook.py
+++ hook.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import re
 
 app = Flask(__name__)
 
@@ -11,7 +12,18 @@ def webhook():
             return jsonify({"output": res["input"]})
         
         badania = request.get_json("https://letsplay.ag3nts.org/data/badania.json")
-        return badania
+        
+        matched = []
+        if isinstance(badania, list):
+            for item in badania:
+                nazwa = item.get("nazwa", "")
+                if isinstance(nazwa, str) and re.search(r"(czas[a-z]{0,2})", nazwa.lower()) and re.search(r"(podróż[a-z]{0,4})", nazwa.lower()):
+                    matched.append(item.get("uczelnia", ""))
+                    matched.append(item.get("sponsor", ""))
+
+        return jsonify({"uczelnia": matched[0],
+                        "sponsor": matched[1]})
+
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
