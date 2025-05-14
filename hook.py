1ba90ebd68f07e6cbaa4259987bede99eaf17b70
Adokr <aj.okrasinsk@student.uw.edu.pl>
2025-05-14 19:16:16 +0200

    hm

--- hook.py
+++ hook.py
@@ -1,55 +1,56 @@
 from flask import Flask, request, jsonify
 import re
+import requests
 
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    try:
-        res = request.get_json()
+    res = request.get_json(silent=True)
+    if res and "input" in res:
+        return jsonify({"output": res["input"]}), 200
+    else:
+        try:
+            res = requests.get("https://letsplay.ag3nts.org/data/badania.json")
+            res.raise_for_status()
+            badania = res.json()
+            matched = []
+            if isinstance(badania, list):
+                for item in badania:
+                    nazwa = item.get("nazwa", "")
+                    if isinstance(nazwa, str) and re.search(r"(czas[a-z]{0,2})", nazwa.lower()) and re.search(r"(podróż[a-z]{0,4})", nazwa.lower()):
+                        matched.append(item.get("uczelnia", ""))
+                        matched.append(item.get("sponsor", ""))
+            print(matched)
+            return jsonify({"uczelnia": matched[0],
+                                "sponsor": matched[1]})
 
-        if "input" in res:
-            return jsonify({"output": res["input"]}), 200
-        
-        badania = request.get_json("https://letsplay.ag3nts.org/data/badania.json")
-        
-        matched = []
-        if isinstance(badania, list):
-            for item in badania:
-                nazwa = item.get("nazwa", "")
-                if isinstance(nazwa, str) and re.search(r"(czas[a-z]{0,2})", nazwa.lower()) and re.search(r"(podróż[a-z]{0,4})", nazwa.lower()):
-                    matched.append(item.get("uczelnia", ""))
-                    matched.append(item.get("sponsor", ""))
-
-        return jsonify({"uczelnia": matched[0],
-                        "sponsor": matched[1]})
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
 
 @app.route('/webhook2', methods=['POST'])
 def webhook2():
-    try:
-        res = request.get_json()
+    res = request.get_json(silent=True)
+    if res and "input" in res:
+        return jsonify({"output": res["input"]}), 200
+    
+    else: 
+        try:
+            if res["params"] != "":
+                szukana_uczelnia = res["params"]
+                res2 = requests.get("https://letsplay.ag3nts.org/data/osoby.json")
+                osoby = res2.json()
+                matched = []
+                if isinstance(osoby, list):
+                    for item in osoby:
+                        uczelnia = item.get("uczelnia", "")
+                        if szukana_uczelnia == uczelnia:
+                            matched.append({"imie": item.get("imie", ""), "nazwisko": item.get("nazwisko", "")})
 
-        if "input" in res:
-            return jsonify({"output": res["input"]}), 200
-        
-        osoby = request.get_json("https://letsplay.ag3nts.org/data/osoby.json")
-        
-        if "uczelnia" in res:
-            uczelnia = res["uczelnia"]
-            matched = {}
-            if isinstance(osoby, list):
-                for item in osoby:
-                    uniwerek = item.get("uczelnia", "")
-                    if uniwerek == uczelnia:
-                        matched.append({"imie": item.get("imie", ""), "nazwisko": item.get("nazwisko", "")})
+                    return jsonify(matched)
 
-            return jsonify(matched)
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
+        except Exception as e:
+            return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8000)
+    print(app.run(host="0.0.0.0", port=8000))
